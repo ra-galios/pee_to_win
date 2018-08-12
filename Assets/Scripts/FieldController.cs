5e018564978c6ac8e158369cc34a94c6c56a02fc
azumangaa <glebshishkanov@gmail.com>
2018-08-12 13:38:37 +0300

    HexMap implementation

---
+++ Assets/Scripts/FieldController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FieldController : MonoBehaviour
+{
+	private struct HexMetrics
+	{
+		public float innerRadius;
+		public float outerRadius;
+	}
+
+	public int side = 3;
+	
+	public HexCell cellPrefab;
+	public Canvas canvas;
+	
+
+	private HexMetrics _hexMetrics;
+	private HexCell[,] _field;
+
+	private void Awake()
+	{
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		RectTransform cellRect = cellPrefab.GetComponent<RectTransform>();
+		_hexMetrics.innerRadius = cellRect.rect.height / 2f;
+		_hexMetrics.outerRadius = cellRect.rect.width / 2f;
+		
+		RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+
+		float fieldSide = canvasRect.rect.width;
+		
+		float hexHeight = (1.5f * side - 0.5f) * (_hexMetrics.outerRadius * 2);
+		float scale = fieldSide / hexHeight;
+		canvas.GetComponent<CanvasScaler>().scaleFactor = scale;
+		
+		Vector2 baseTransform = new Vector2();
+		baseTransform.x = - canvasRect.rect.width / 2 / scale;
+		baseTransform.y = - canvasRect.rect.height / 2 / scale;
+
+		int width = side * 2 - 1;
+		int height = width * 2;
+		_field = new HexCell[width, height];
+		
+		for(int level = 0; level < width; level++)
+		{
+			int validRowNumber = 0;
+			for (int idx = 0; idx < height; idx++)
+			{
+				HexCell cell = createCell(baseTransform, level, idx);
+				if (cell != null)
+				{
+					cell.setIJ(_field, level, validRowNumber);
+					_field[level, validRowNumber] = cell;
+					validRowNumber++;
+				}
+			}
+		}
+
+		int i = 1 + 2;
+	}
+
+	private HexCell createCell(Vector2 baseTransform, int i, int j)
+	{
+		if (i % 2 == j % 2)
+		{
+			Vector3 position = new Vector3(baseTransform.x, baseTransform.y);
+			position.x += (i  / 2f * 3f + 1) * _hexMetrics.outerRadius;
+			position.y += (j + 1) * _hexMetrics.innerRadius;
+
+			HexCell cell = Instantiate<HexCell>(cellPrefab);
+			cell.transform.SetParent(transform, false);
+			cell.transform.localPosition = position;
+			return cell;
+		}
+
+		return null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+}
---
+++ Assets/Scripts/HexCell.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexCell : MonoBehaviour
+{
+	protected enum NBRS
+	{
+		TOP,TOPRIGHT,BOTRIGHT,BOT,BOTLEFT,TOPLEFT,SIZE,INVALID
+	}
+	
+	private int i, j;
+	private HexCell[] neighbours = new HexCell[(int)NBRS.SIZE];
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setIJ(HexCell[,] field, int i, int j)
+	{
+		this.i = i;
+		this.j = j;
+
+		for (int d = 0; d < (int) NBRS.SIZE; d++)
+		{
+			Vector2 nbrPos = desideIJ((NBRS) d);
+			if (nbrPos.x >= 0 && nbrPos.x < field.Rank)
+			{
+				if (nbrPos.y >= 0 && nbrPos.y < field.GetLength((int) nbrPos.x))
+				{
+					HexCell cell = field[(int) nbrPos.x, (int) nbrPos.y];
+					if (cell != null)
+					{
+						cell.addNeighbour(this);
+					}
+				}
+			}
+		}
+		
+		transform.GetChild(0).gameObject.GetComponent<Text>().text = i + ";" + j;
+	}
+
+	protected Vector2 desideIJ(NBRS direction)
+	{
+		Vector2 ret = new Vector2(-1, -1);
+		int top = j + i % 2;
+		int bot = top - 1;
+		
+		switch (direction)
+		{
+			case NBRS.TOP:
+				ret.x = i;
+				ret.y = j + 1;
+				break;
+			case NBRS.TOPRIGHT:
+				ret.x = i + 1;
+				ret.y = top;
+				break;
+			case NBRS.BOTRIGHT:
+				ret.x = i + 1;
+				ret.y = bot;
+				break;
+			case NBRS.BOT:
+				ret.x = i;
+				ret.y = j - 1;
+				break;
+			case NBRS.BOTLEFT:
+				ret.x = i - 1;
+				ret.y = bot;
+				break;
+			case NBRS.TOPLEFT:
+				ret.x = i - 1;
+				ret.y = top;
+				break;
+		}
+
+		return ret;
+	}
+	
+	
+	protected NBRS desideNBR(HexCell cell)
+	{
+		if (cell.getI() == i)
+		{
+			int vDelta = cell.getJ() - j;
+			switch (vDelta)
+			{
+					case 1:
+						return NBRS.TOP;
+					case 2:
+						return NBRS.BOT;
+			}
+		}
+		
+		int top = j + i % 2;
+		int bot = top - 1;
+		if (cell.getJ() == top)
+		{
+			int horDelta = i - cell.getI();
+			if (horDelta == -1)
+				return NBRS.TOPLEFT;
+			if (horDelta == 1)
+				return NBRS.TOPRIGHT;
+		
+			return NBRS.INVALID;
+		}
+
+		if (cell.getJ() == bot)
+		{
+			int horDelta = i - cell.getI();
+			if (horDelta == -1)
+				return NBRS.BOTLEFT;
+			
+			if (horDelta == 1)
+				return NBRS.BOTRIGHT;
+			
+			return NBRS.INVALID;
+		}
+
+		return NBRS.INVALID;
+	}
+
+	protected int getI()
+	{
+		return i;
+	}
+
+	protected int getJ()
+	{
+		return j;
+	}
+
+	protected void addNeighbour(HexCell neighbour)
+	{
+		if (neighbour != null && !neighbours.Contains(neighbour))
+		{
+			NBRS direction = desideNBR(neighbour);
+			if (direction < NBRS.SIZE)
+			{
+				neighbours[(int) direction] = neighbour;
+				neighbour.addNeighbour(this);
+			}
+		}
+	}
+
+	protected void remove(HexCell neighbour)
+	{
+		if (neighbour != null && neighbours.Contains(neighbour))
+		{
+			NBRS direction = desideNBR(neighbour);
+			neighbours[(int) direction] = null;
+			neighbour.remove(this);
+		}
+	}
+}
